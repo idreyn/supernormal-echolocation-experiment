cfe7e21375e00d70ec5940e88c09aa060da05576
Ian Reynolds <idreyn@gmail.com>
2021-01-09 14:53:17 -0500

    pull before force pushing when deploying

--- build/deploy.js
+++ build/deploy.js
@@ -28,6 +28,7 @@ const main = () => {
         execHere(`git remote add origin ${remoteUrl}`);
         execHere(`git fetch`);
     }
+    execHere(`git pull`);
     execHere(`git add .`);
     execHere(`git commit -am "Deployment on ${new Date().toString()}" --allow-empty`);
     execHere(`git branch --set-upstream-to origin/${branch}`);
